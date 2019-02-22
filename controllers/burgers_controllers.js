53e49a4fcdf5a48ee584e764a95bfbde1e75d47c
Skarrg <orestes.moiran@gmail.com>
2019-02-21 20:15:30 -0800

    finally

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -18,44 +18,22 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burgers.insertOne([
-    "name", "devoured"
+    "burger_name", "devoured_bool"
   ], [
-    req.body.name, req.body.sleepy
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+    req.body.burger_name, false
+  ], function() {
+    res.redirect("/");
   });
 });
 
-// router.put("/api/burgers/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
+router.post("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
 
-//   console.log("condition", condition);
-
-//   cat.update({
-//     sleepy: req.body.sleepy
-//   }, condition, function(result) {
-//     if (result.changedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
-
-// router.delete("/api/cats/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
-
-//   cat.delete(condition, function(result) {
-//     if (result.affectedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
-
+  burgers.updateOne({
+    devoured_bool: req.body.devoured
+  }, condition, function(data) {
+    res.redirect("/");
+  });
+});
 // Export routes for server.js to use.
 module.exports = router;
